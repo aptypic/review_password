a1465995e43d4e908408c8f2b8edbb714068f35f
asivolap <aptypic@gmail.com>
2021-06-25 16:25:32 +0300

    тест

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-print("Введите пароль: pbwDWnd2")
 user_input = input()
 score = 0
 
@@ -7,31 +6,35 @@ def check_digital(user_input):
     return any(letter.isdigit() for letter in user_input)
 
 
-def check_length(user_input):
-    if len(user_input) < 12:
-        return True
-
-
 def check_letters(user_input):
     return any(letter.isalpha() for letter in user_input)
 
 
+def check_upper_letters(user_input):
+    return any(letter.isupper() for letter in user_input)
+
+
+def check_length(user_input):
+    if len(user_input) > 12:
+        return True
+
+
+def check_symbols(user_input):
+    return any((letter.isdigit() == False and letter.isalpha() == False) for letter in user_input)
+
+
+def check_lower_letters(user_input):
+    return any(letter.islower() for letter in user_input)
+
+
+funcs_dict = [check_length(user_input), check_digital(user_input), check_letters(user_input),
+              check_lower_letters(user_input), check_upper_letters(user_input), check_symbols(user_input)]
+
+
 def main(score):
-    if check_letters(user_input):
-        print("Есть буквы")
-        score += 2
-    else:
-        print("Нет букв")
-    if check_digital(user_input):
-        print("Есть цифры")
-        score += 2
-    else:
-        print("Нет цифр")
-    if check_length(user_input):
-        print("Короткий")
-    else:
-        print("Длинный")
-        score += 2
+    for func in funcs_dict:
+        if func:
+            score += 2
     return score
 
 
